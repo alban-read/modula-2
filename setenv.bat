b2800e6a332ed742bfc8fa37b4286faf0a76144b
alban read <alban.read@googlemail.com>
2019-08-18 17:11:43 +0100

    batch file

---
+++ setenv.bat
@@ -0,0 +1,5 @@
+
+"%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86 8.1
+set INCLUDE=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Include;%INCLUDE%
+set PATH=S:\projects\xds\XDS\bin;%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Bin;%PATH%
+set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib;%LIB%
